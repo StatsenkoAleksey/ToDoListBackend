d8ab1e48f53e8e6260a83805f9ac4bb325dad020
StatsenkoAleksey <64027378+StatsenkoAleksey@users.noreply.github.com>
2021-07-01 20:38:20 +0300

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,8 @@ module ToDoServer
 
     config.action_dispatch.default_headers.merge!({
       'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => '*'
+      'Access-Control-Request-Method' => '*',
+      'Access-Control-Allow-Headers' => '*'
     })
     
     # Configuration for the application, engines, and railties goes here.
